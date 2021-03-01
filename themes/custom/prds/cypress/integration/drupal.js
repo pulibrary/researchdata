59d722d05374eea17d3de0fdfa5903f20891c382
Axa Mei Liauw <aliauw@princeton.edu>
2021-03-01 12:28:12 -0500

    Adds axe to cypress for automated accessibility

--- themes/custom/prds/cypress/integration/drupal.js
+++ themes/custom/prds/cypress/integration/drupal.js
@@ -1,14 +1,20 @@
 context('anon', function() {
     beforeEach(function() {
         cy.visit('/');
+        cy.injectAxe();
     })
 
-    it('site loads', function() {
-        cy.visit('/');
+    it('page is accessible', function() {
+        cy.checkA11y(
+            {
+                exclude: ['.skip-link']
+            }
+        );
+
         cy.percySnapshot('Homepage', { widths: [1280, 1440, 1680, 1920]});
     })
 
-    it('site has correct title', function() {
+    it('page has correct title', function() {
         cy.title().should('eq', 'Landing Page | Princeton Research Data Service')
     })
 
