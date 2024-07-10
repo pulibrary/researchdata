8ecf7447322851429aa1569655b866c61155533c
Francis Kayiwa <kayiwa@pobox.com>
2024-07-10 11:06:46 -0400

    add a second set of vms
    this allows us to be able to do A/B testing
    
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
 set :branch, ENV["BRANCH"] || "main"
 
 server "prds-prod1", user: fetch(:user), roles: %w{app drupal_primary}
+server "prds-prod2", user: fetch(:user), roles: %w{app drupal_primary}
 
 #set :search_api_solr_host, 'lib-solr-staging.princeton.edu'
 #set :search_api_solr_path, '/solr/recap-staging'
